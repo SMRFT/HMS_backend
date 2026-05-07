5f225f37c5be1bdaad57ebb5e2f992529a859d1f
manibalan (SMRFT) <manibalansmrft@gmail.com>
2026-05-07 13:46:07 +0530

    Delete force_close.py

--- force_close.py
+++
@@ -1,3 +0,0 @@
-from hospital.models import Cashcountershiftdetails
-res = Cashcountershiftdetails.objects.filter(CashCounter="OLET003", ShiftStatus="active").update(ShiftStatus="inactive", is_active=False)
-print(f"Force closed {res} shifts via Django shell")
